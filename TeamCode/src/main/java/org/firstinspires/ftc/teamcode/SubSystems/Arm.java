938d1ddc14613ff90c2311abdf6a90134a63d1c1
DrChz <raymund.lin54@gmail.com>
2023-11-13 15:03:01 -0600

    Create Arm.java
    started on arm, many errors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Arm.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.SubSystems;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Arm {
+    private GamepadEx driver, operator;
+    private Servo armLeft;
+    private Servo armRight;
+    int armTarget = 0;
+    double armSpeed = 0;
+    String armCurrentDirection = "up";
+    public Arm(HardwareMap hardwareMap){
+
+        armLeft = hardwareMap.servo.get("armLeft");
+        armRight = hardwareMap.servo.get("armRight");
+
+        // Reverse backwards arm motor
+        armRight.setDirection(Servo.Direction.REVERSE);
+
+        //get starting position
+        armLeft.getPosition();
+        armRight.getPosition();
+
+        // Set arm encoders to 0
+        armLeft.setPosition(0);
+        armRight.setPosition(0);
+
+
+        public void upanddown(GamepadEx gamepad1)
+        {
+            if (driver.wasJustPressed(GamepadKeys.Button.A)){ // Arm UP
+                armTarget = 200;
+                armSpeed = 0.98;
+                armCurrentDirection = "up";
+
+                armLeft.setPower(armSpeed);
+                armLeft.setPosition(armTarget);
+
+            } else if (driver.wasJustPressed(GamepadKeys.Button.B)) { // Arm DOWN
+                armTarget = 0;
+                armSpeed = -0.6;
+                armCurrentDirection = "down";
+
+                armLeft.setPower(armSpeed);
+                armLeft.setTargetPosition(armTarget);
+            }
+        }
+        // Setup Telemetry, will not clear after cycle, setup reusable items for output
+    }
+
+}
+
