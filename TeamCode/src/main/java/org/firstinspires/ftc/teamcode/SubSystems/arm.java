26cb4480ee991cddc21657dda2266543bfaecc07
Adii70 <fire.slayer7gold@gmail.com>
2023-11-16 17:34:25 -0600

    lil arm work
    etgerg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/arm.java
@@ -3,32 +3,39 @@ package org.firstinspires.ftc.teamcode.SubSystems;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-
+import com.qualcomm.robotcore.hardware.CRServo;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.states.armState;
 
 public class arm {
-    private Servo leftArm, rightArm;
+
+    private CRServo leftArm,rightArm;
+    Telemetry tel;
     public arm (HardwareMap hardwareMap, Telemetry telemetry)
     {
-        Telemetry tel = telemetry;
-        leftArm = hardwareMap.servo.get("leftArm");
-        rightArm = hardwareMap.servo.get("rightArm");
+        tel = telemetry;
+        leftArm = hardwareMap.crservo.get("leftArm");
+        rightArm = hardwareMap.crservo.get("rightArm");
 
 
 
     }
 
     public void setPosition(armState state){
-
+        tel.addLine("#1");
+        tel.update();
         switch (state){
             case intaking:
-                leftArm.setPosition(-0.5);
-                rightArm.setPosition(0.5);
+                tel.addLine("#2");
+                tel.update();
+                leftArm.setPower(.65);
+                rightArm.setPower(.45);
+                //Thread.sleep(5000);
+
                 break;
             case outtaking:
-                leftArm.setPosition(0.8);
-                rightArm.setPosition(-0.8);
+                leftArm.setPower(.8);
+                rightArm.setPower(-.80);
                 break;
         }
     }
