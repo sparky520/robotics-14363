dc97d506ca28f9ca170ed2a77f385ca26442c6c8
RajarshiBandhu <rajarshibandhu@gmail.com>
2023-11-28 13:50:12 -0800

    gjfyh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/LeftBluePark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/LeftBluePark.java
@@ -1,22 +1,11 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
-public class LeftBluePark extends LinearOpMode{
-    //current position
-    Pose2d myPose = new Pose2d(-63, 12, Math.toRadians(-180));
-    public void runOpMode(){
-        SampleMecanumDrive park = new SampleMecanumDrive(hardwareMap);
+import org.firstinspires.ftc.teamcode.drive.mecanumDrive;
+public class leftBluePark {
 
-        park.setPoseEstimate(myPose);
-
-        Trajectory trajectory = park.trajectoryBuilder(myPose).strafeRight(50).build();
-
-        if (isStopRequested()) return;
-
-        park.followTrajectory(trajectory);
-
-    }
 }
