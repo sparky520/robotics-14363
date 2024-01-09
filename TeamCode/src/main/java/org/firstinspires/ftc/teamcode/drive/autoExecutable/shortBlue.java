36d96290d83086fe90aea80e28a1799524d06c5d
RajarshiBandhu <97370250+RajarshiBandhu@users.noreply.github.com>
2024-01-08 18:40:48 -0600

    ssdgsdg
    sdg

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/autoExecutable/shortBlue.java
@@ -0,0 +1,204 @@
+package org.firstinspires.ftc.teamcode.drive.autoExecutable;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.firstinspires.ftc.teamcode.drive.auto.objdetect_blue;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys.*;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.firstinspires.ftc.teamcode.states.*;
+import org.firstinspires.ftc.teamcode.SubSystems.*;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.SubSystems.arm;
+
+@Autonomous(name = "short Blue", group = "Auto")
+public class shortBlue extends LinearOpMode {
+    OpenCvCamera camera;
+    objdetect_blue blueDetection;
+    String webcamName;
+    Robot robot;
+    public void runOpMode() {
+        robot = new Robot(hardwareMap, telemetry);
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d newStart = new Pose2d();
+        TrajectorySequence center = drive.trajectorySequenceBuilder(newStart)
+                .addDisplacementMarker(() -> {
+                    robot.Claw.setPosition(armState.intakingCLAW);
+                })
+                .lineToLinearHeading(new Pose2d(36.25,0,Math.toRadians(-90)))
+                .addDisplacementMarker(() -> {
+                    robot.Claw.setTape();
+                    robot.Arm.setPosition(armState.medium);
+                })
+                .lineToConstantHeading(new Vector2d(36,9.01))
+                .waitSeconds(.25)
+                .addDisplacementMarker(() -> {
+                    robot.slide.setOuttakeSlidePosition(outtakeStates.etxending, outtakeStates.HIGHIN);
+                    robot.Arm.setPosition(armState.high);
+                })
+                .lineToConstantHeading(new Vector2d(31,9.02))
+                .waitSeconds(1)
+                .lineToConstantHeading(new Vector2d(28,30))
+                .addDisplacementMarker(() -> {
+                    robot.Claw.dropBoard();
+                })
+                .lineToConstantHeading(new Vector2d(20,18))
+                //.waitSeconds(1)
+                .addDisplacementMarker(() -> {
+                    robot.Arm.setPosition(armState.medium);
+                    robot.Claw.setPosition(armState.intakingCLAW);
+                    robot.slide.setOuttakeSlidePosition(outtakeStates.etxending, outtakeStates.STATION);
+                })
+                .lineToConstantHeading(new Vector2d(0,23))
+                .addDisplacementMarker(() -> {
+                    robot.Arm.setPosition(armState.low);
+                })
+                .lineToConstantHeading(new Vector2d(0,30))
+                .build();
+        TrajectorySequence left = drive.trajectorySequenceBuilder(newStart)
+                .addDisplacementMarker(() -> {
+                    robot.Claw.setPosition(armState.intakingCLAW);
+                })
+                .lineToLinearHeading(new Pose2d(31,9,Math.toRadians(-90)))
+                .addDisplacementMarker(() -> {
+                    robot.Claw.setTape();
+                    robot.Arm.setPosition(armState.medium);
+                })
+                .lineToConstantHeading(new Vector2d(31,9.01))
+                .waitSeconds(.25)
+                .addDisplacementMarker(() -> {
+                    robot.slide.setOuttakeSlidePosition(outtakeStates.etxending, outtakeStates.HIGHIN);
+                    robot.Arm.setPosition(armState.high);
+                })
+                .lineToConstantHeading(new Vector2d(31,9.02))
+                .waitSeconds(1)
+                .lineToConstantHeading(new Vector2d(23,29.25))
+                .addDisplacementMarker(() -> {
+                    robot.Claw.dropBoard();
+                })
+                .lineToConstantHeading(new Vector2d(20,18))
+                //.waitSeconds(1)
+                .addDisplacementMarker(() -> {
+                    robot.Arm.setPosition(armState.medium);
+                    robot.Claw.setPosition(armState.intakingCLAW);
+                    robot.slide.setOuttakeSlidePosition(outtakeStates.etxending, outtakeStates.STATION);
+                })
+                .lineToConstantHeading(new Vector2d(0,23))
+                .addDisplacementMarker(() -> {
+                    robot.Arm.setPosition(armState.low);
+                })
+                .lineToConstantHeading(new Vector2d(0,30))
+                .build();
+        TrajectorySequence right = drive.trajectorySequenceBuilder(newStart)
+                .addDisplacementMarker(() -> {
+                    robot.Claw.setPosition(armState.intakingCLAW);
+                })
+                .lineToLinearHeading(new Pose2d(28,6,Math.toRadians(-90)))
+                .lineToConstantHeading(new Vector2d(28,-11))
+                .addDisplacementMarker(() -> {
+                    robot.Claw.setTape();
+                    robot.Arm.setPosition(armState.medium);
+                })
+                .lineToConstantHeading(new Vector2d(28,-10.99))
+                .waitSeconds(.25)
+                .addDisplacementMarker(() -> {
+                    robot.slide.setOuttakeSlidePosition(outtakeStates.etxending, outtakeStates.HIGHIN);
+                    robot.Arm.setPosition(armState.high);
+                    robot.Claw.setPosition(armState.intakingCLAW);
+                })
+                //.lineToConstantHeading(new Vector2d(31,9.02))
+                //.waitSeconds(1)
+                .lineToConstantHeading(new Vector2d(33.75,30.75))
+                .addDisplacementMarker(() -> {
+                    robot.Claw.dropBoard();
+                })
+                .lineToConstantHeading(new Vector2d(33,18))
+                //.waitSeconds(1)
+                .addDisplacementMarker(() -> {
+                    robot.Arm.setPosition(armState.medium);
+                    robot.Claw.setPosition(armState.intakingCLAW);
+                    robot.slide.setOuttakeSlidePosition(outtakeStates.etxending, outtakeStates.STATION);
+                })
+                .lineToConstantHeading(new Vector2d(0,23))
+                .addDisplacementMarker(() -> {
+                    robot.Arm.setPosition(armState.low);
+                })
+                .lineToConstantHeading(new Vector2d(0,30))
+                .build();
+        initCam();
+        waitForStart();
+        camera.stopStreaming();
+        if (isStopRequested()) return;
+        if (blueDetection.getLocation().equals("LEFT")){
+            drive.followTrajectorySequence(left);
+        }else if(blueDetection.getLocation().equals("MIDDLE")){
+            drive.followTrajectorySequence(center);
+        }else if(blueDetection.getLocation().equals("RIGHT")){
+            drive.followTrajectorySequence(right);
+        }
+        else{
+            telemetry.addLine("wrong" + blueDetection.getLocation());
+        }
+        telemetry.update();
+    }
+    private void initCam() {
+
+        //This line retrieves the resource identifier for the camera monitor view. The camera monitor view is typically used to display the camera feed
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        webcamName = "Webcam 1";
+
+        // This line creates a webcam instance using the OpenCvCameraFactor with the webcam name (webcamName) and the camera monitor view ID.
+        // The camera instance is stored in the camera variable that we can use later
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, webcamName), cameraMonitorViewId);
+
+        // initializing our Detection class (details on how it works at the top)
+        blueDetection = new objdetect_blue(telemetry);
+
+        // yeah what this does is it gets the thing which uses the thing so we can get the thing
+        /*
+        (fr tho idk what pipeline does, but from what I gathered,
+         we basically passthrough our detection into the camera
+         and we feed the streaming camera frames into our Detection algorithm)
+         */
+        camera.setPipeline(blueDetection);
+
+        /*
+        this starts the camera streaming, with 2 possible combinations
+        it starts streaming at a chosen res, or if something goes wrong it throws an error
+         */
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.showFpsMeterOnViewport(true);
+                camera.startStreaming(320, 240, OpenCvCameraRotation.SENSOR_NATIVE);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addLine("Unspecified Error Occurred; Camera Opening");
+            }
+        });
+    }
+}
