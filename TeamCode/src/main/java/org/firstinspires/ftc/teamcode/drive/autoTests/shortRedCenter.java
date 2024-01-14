3131e2d0b42be2e1e0b0eaab90b5493ce6e1ac76
RajarshiBandhu <97370250+RajarshiBandhu@users.noreply.github.com>
2024-01-14 12:52:56 -0600

    shortRedCenter re-written

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/autoTests/shortRedCenter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/autoTests/shortRedCenter.java
@@ -2,8 +2,9 @@ package org.firstinspires.ftc.teamcode.drive.autoTests;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.SubSystems.Robot;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
@@ -16,11 +17,24 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 public class shortRedCenter extends LinearOpMode
 {
     Robot robot;
+    enum state{
+        toCenter,
+        wait1,
+        wait2,
+        toBoard,
+        wait3,
+        moveAwayFromBoard,
+        retractArm1,
+        retractArm2,
+        IDLE,
+    }
+    state currentState = state.IDLE;
     @Override
     public void runOpMode() {
         DistanceSensor distanceSensor = hardwareMap.get(DistanceSensor.class, "distancesensor");
         robot = new Robot(hardwareMap, telemetry);
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        ElapsedTime timer = new ElapsedTime();
 
         Pose2d start = new Pose2d();
         TrajectorySequence center1 = drive.trajectorySequenceBuilder(start)
@@ -53,13 +67,78 @@ public class shortRedCenter extends LinearOpMode
                     robot.Claw.stack();
                 })
                 .build();
+
+        Trajectory center = drive.trajectoryBuilder(start).lineToLinearHeading(new Pose2d(38.5,-3,Math.toRadians(90))).build();
+        Trajectory board = drive.trajectoryBuilder(center.end()).lineToConstantHeading(new Vector2d(21,-36)).build();
+        Trajectory moveAwayFromBoard = drive.trajectoryBuilder(board.end()).lineToConstantHeading(new Vector2d(21,-29)).build();
         waitForStart();
 
         if(isStopRequested()) return;
-        drive.followTrajectorySequence(center1);
 
+        currentState = state.toCenter;
+        drive.followTrajectoryAsync(center);
 
+        while (opModeIsActive() && !isStopRequested()) {
+            switch(currentState){
+                case toCenter:
+                    if (!drive.isBusy()){
+                        currentState = state.wait1;
+                        robot.Claw.setTape();
+                        timer.reset();
+                    }
+                    break;
+                case wait1:
+                    if (timer.seconds() > .3){
+                        robot.Arm.setPosition(armState.medium);
+                        currentState = state.wait2;
+                    }
+                    break;
+                case wait2:
+                    if (timer.seconds() > .6){
+                        currentState = state.toBoard;
+                        robot.Arm.setPosition(armState.high);
+                        robot.slide.setOuttakeSlidePosition(outtakeStates.etxending, outtakeStates.HIGHIN);
+                        timer.reset();
+                    }
+                case toBoard:
+                    if (timer.seconds() > .5){
+                        currentState = state.wait3;
+                        drive.followTrajectoryAsync(board);
+                    }
+                    break;
+                case wait3:
+                    if (!drive.isBusy()){
+                        robot.Claw.dropBoard();
+                        currentState = state.moveAwayFromBoard;
+                        timer.reset();
+                    }
+                    break;
+                case moveAwayFromBoard:
+                    if (timer.seconds() > .3) {
+                        drive.followTrajectoryAsync(moveAwayFromBoard);
+                        currentState = state.retractArm1;
+                    }
+                    break;
+                case retractArm1:
+                    if (!drive.isBusy()){
+                        robot.slide.setOuttakeSlidePosition(outtakeStates.etxending, outtakeStates.STATION);
+                        robot.Arm.setPosition(armState.medium);
+                        robot.Claw.setPosition(armState.intakingCLAW);
+                        currentState = state.retractArm2;
+                        timer.reset();
+                    }
+                    break;
+                case retractArm2:
+                    if (timer.seconds() > .5){
+                        robot.Arm.topStack();
+                        currentState = state.IDLE;
+                    }
+                case IDLE:
+                    break;
+            }
+        }
 
-
+        drive.update();
+        telemetry.addData("state", currentState);
     }
 }
