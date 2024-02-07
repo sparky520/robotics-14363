6e477ee38df42c307cca704dfd21cbe20b0c7af8
Adii70 <fire.slayer7gold@gmail.com>
2024-02-06 21:41:23 -0600

    hdajkfhasjkfh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Mecanum.java
@@ -16,6 +16,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
@@ -28,6 +30,12 @@ public class Mecanum
     BNO055IMU.Parameters parameters;
     private double x, y, rx, rotX, rotY, denominator, frontLeftPower, backLeftPower, frontRightPower, backRightPower;
     public double slow_mode, botHeading ;
+    private double lastError = 0;
+    ElapsedTime timer = new ElapsedTime();
+    double integralSum = 0;
+    double kp = 0.02;
+    double ki = 0;
+    double kd = 1; 
 
 
 
@@ -86,6 +94,54 @@ public class Mecanum
         backLeftMotor.setPower(backLeftPower*slow_mode);
         frontRightMotor.setPower(frontRightPower *slow_mode);
         backRightMotor.setPower(backRightPower*.885*slow_mode);
+    }
+    public double wrap(double angle){
+        if (angle > 180){
+            angle -= 360;
+        }
+        else if (angle < -180){
+            angle += 360;
+        }
+        return angle;
+    }
+    public double small(double target, double current){
+        current = wrap(current);
+        target = wrap(target);
+        double dif = target - current;
+        if (dif < -180){
+            dif += 360;
+        }
+        else if (dif > 180){
+            dif = -(360-dif);
+        }
+        return dif;
+    }
+    public void angleLock(GamepadEx  driver, double angle){
+        y = driver.getLeftY();
+        x = driver.getLeftX();
+        double error = small(angle ,imu.getAngularOrientation().firstAngle * (180/Math.PI));
+
+        integralSum += error * timer.seconds();
+        double derivative = -(wrap(error - lastError)) / (timer.seconds());
+        lastError = error;
+
+
+        timer.reset();
+        double output = (error * -kp) + (imu.getAngularVelocity().zRotationRate * kd) + (integralSum * ki);
+        double rx = output;
+
+        double botHeading = -imu.getAngularOrientation().firstAngle;
+
+
+
+        rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+        rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
+
+        denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        frontLeftPower = (rotY + rotX + rx) / denominator;
+        backLeftPower = (rotY - rotX + rx) / denominator;
+        frontRightPower = (rotY - rotX - rx) / denominator;
+        backRightPower = (rotY + rotX - rx) / denominator;
     }
 
 
